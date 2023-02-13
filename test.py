3458c38fc14a0df443fba89cff52c35803d78e59
MeryembentalebEJ <meryembentaleb11@gmail.com>
2023-02-13 12:29:16 +0100

    add a simple function using python

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
 print("Hello, World!")
+def rectangle_area(length, width):
+    return length * width
+
+rect_length = 5
+rect_width = 3
+rect_area = rectangle_area(rect_length, rect_width)
+
+print("The area of a rectangle with length", rect_length, "and width", rect_width, "is", rect_area)
